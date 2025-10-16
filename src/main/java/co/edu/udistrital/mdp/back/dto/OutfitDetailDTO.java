b773db2a2bf1a299bfe94532fd8f170bb09f1c8a
Erik2626 <ejfernandezr@udistrital.edu.co>
2025-10-16 08:46:51 -0500

    Update OutfitDetailDTO.java

--- src/main/java/co/edu/udistrital/mdp/back/dto/OutfitDetailDTO.java
+++ src/main/java/co/edu/udistrital/mdp/back/dto/OutfitDetailDTO.java
@@ -8,6 +8,6 @@ import lombok.Getter;
 import lombok.Data;
 @Data
 public class OutfitDetailDTO extends OutfitDTO {
-//private List<ListaDeseosDTO> listadeseos = new ArrayList<>();
+private List<ListaDeseosDTO> listadeseos = new ArrayList<>();
 private List<RecomendacionDTO> recomendacion = new ArrayList<>();
 }
